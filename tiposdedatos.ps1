707ff715561f52f491a6d5e4beba1f92357a38ce
javier-ctrlz <javier.zapien237@gmail.com>
2025-02-05 23:22:52 -0700

    Scripts del manual
    Se subieron las primeras dos unidades del manual

---
+++ tiposdedatos.ps1
@@ -0,0 +1,6 @@
+#Ilustración 11
+[int]$variable1=100
+[int]$variable2="Hola"
+
+#Ilustración 12
+$variable1.GetType()
---
+++ variables_y_tiposdedatos.ps1
@@ -0,0 +1,53 @@
+#Ilustración 1
+$variable1="Hola"
+
+$variable2="Que tal?"
+
+$variable3=100
+
+${VAR iable4}=200
+
+#Ilustración 2
+New-Variable -Name variable5 -Value 300
+
+$variable5
+
+#Ilustración 3
+$variable1
+
+$variable2
+
+$variable3
+
+${VAR iable4}
+
+#Ilustración 4
+$variable1 + $variable2
+
+$variable3 + ${VAR iable4}
+
+$variable3 - ${VAR iable4}
+
+#Ilustración 5
+$variable1 + $variable2
+
+$$
+
+#Ilustración 6
+$variable1 + $variable2
+
+$^
+
+#Ilustración 7
+$variable1 + $variable2
+
+$?
+
+#Ilustración 8
+$Error
+
+#Ilustración 9
+Get-Help about_automatic_variables
+
+#Ilustración 10
+Get-Help about_preference_variables
